7424af97c67992895dd68cb964ea8bafba200473
Lucas Michals <lucasmichals@Lucass-MacBook-Pro.local>
2018-11-22 14:24:15 -0800

    pull out grid presenter props

--- src/components/Grid/Presenter.js
+++ src/components/Grid/Presenter.js
@@ -1,30 +1,20 @@
 import React from "react";
 import Column from "./children/Column";
 
-const gridProps = {
-  cellLength: 40,
-  gridSize: [10, 20],
-  dotsVisible: true,
-  dotProps: {
-    color: "#fff",
-    diameter: 5
-  },
-  numbering: false,
-  rootCoords: [15, 25]
+export default ({
+  rootCoords,
+  gridSize,
+  cellProps: { length: cellLength }
+}) => {
+  return (
+    <g transform={`translate(${rootCoords[0]} ${rootCoords[1]})`}>
+      <rect
+        className="background"
+        height={cellLength * gridSize[0]}
+        width={cellLength * gridSize[1]}
+        fill="red"
+      />
+      <Column cellProps={{ length: 20 }} />
+    </g>
+  );
 };
-
-export default () => (
-  <g
-    transform={`translate(${gridProps.rootCoords[0]} ${
-      gridProps.rootCoords[1]
-    })`}
-  >
-    <rect
-      className="background"
-      height={gridProps.cellLength * gridProps.gridSize[0]}
-      width={gridProps.cellLength * gridProps.gridSize[1]}
-      fill="red"
-    />
-    <Column cellProps={{ length: 20 }} />
-  </g>
-);
